fa97d89258e453e52cf0203f1890ff0685444adf
Mr Thirteen <23302010032@m.fudan.edu.cn>
2025-06-14 21:15:24 +0800

    删了一些没用的东西

--- fix-encoding.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-chcp 65001
-for /r %%i in (*.java) do (
-  echo Converting %%i to UTF-8
-  powershell -Command "$content = Get-Content -Path '%%i' -Encoding Default; $content | Set-Content -Path '%%i' -Encoding UTF8"
-)
